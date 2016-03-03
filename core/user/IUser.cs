f6f8bd23670152dbd2dddccbd3541d96189a8f06
Ladislav Sopko <lsopko@3di.it>
2016-03-03 13:23:17 +0100

    Work on User

--- core/user/IUser.cs
+++ core/user/IUser.cs
@@ -1,10 +1,15 @@
-﻿namespace xwcs.core.user
+﻿using System.Collections.Generic;
+
+namespace xwcs.core.user
 {
     public interface IUser
     {
-        string Name { get; }
+        int	ID { get;  }
+		string Name { get; }
         string Login { get; }
         bool isAdmin();
+		bool hasTag();
+		List<string> GetTags();
     }
 }
 
--- core/user/IUserProvider.cs
+++ core/user/IUserProvider.cs
@@ -2,6 +2,7 @@
 {
     public interface IUserProvider
     {
+		IUser getCurrentUser();
     }
 }
 
