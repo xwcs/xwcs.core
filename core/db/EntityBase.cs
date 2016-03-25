c1ee0dfe1fc7b8d88130349a0faf9dbfb2537e16
Ladislav Sopko <lsopko@3di.it>
2016-03-25 19:57:42 +0100

    Started EF adapting using modify of T4 templates

---
+++ core/db/EntityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace xwcs.core.db
+{
+	public abstract class EntityBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (Equals(storage, value)) return false;
+			storage = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var eventHandler = PropertyChanged;
+			if (eventHandler != null)
+			{
+				eventHandler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}	
+}
