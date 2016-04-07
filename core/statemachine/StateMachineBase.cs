e87ec1a7f51f73146321226988a099785968dc98
Ladislav Sopko <lsopko@3di.it>
2016-04-07 10:44:23 +0200

    added state machine

---
+++ core/statemachine/StateMachineBase.cs
@@ -0,0 +1,444 @@
+﻿// Converting C:\xwee\gitRepos\database\support\DocumentStates\DocumentStates\DocumentStates.tastate into .cs file
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace xwcs.core.statemachine
+{
+    #region SyncClasses
+    /// <summary>
+    /// Syncronization Class.
+    /// </summary>
+    public class SyncEvents
+    {
+        public SyncEvents()
+        {
+
+            _newTransitionEvent = new AutoResetEvent(false);
+            _exitThreadEvent = new ManualResetEvent(false);
+            _eventArray = new WaitHandle[2];
+            _eventArray[0] = _newTransitionEvent;
+            _eventArray[1] = _exitThreadEvent;
+        }
+
+        public EventWaitHandle ExitThreadEvent
+        {
+            get { return _exitThreadEvent; }
+        }
+        public EventWaitHandle NewTransitionEvent
+        {
+            get { return _newTransitionEvent; }
+        }
+        public WaitHandle[] EventArray
+        {
+            get { return _eventArray; }
+        }
+
+        private EventWaitHandle _newTransitionEvent;
+        private EventWaitHandle _exitThreadEvent;
+        private WaitHandle[] _eventArray;
+    }
+    #endregion
+
+    #region TransitionClasses
+    /// <summary>
+    /// Transition Event Class.
+    /// </summary>
+    public class TransitionEventArgs : EventArgs
+    {
+        public TransitionEventArgs(StateBase prev, StateBase next, TriggerBase why)
+        {
+            Prev = prev;
+            Next = next;
+            Why = why;
+        }
+
+        public StateBase Prev { get; set; }
+        public StateBase Next { get; set; }
+        public TriggerBase Why { get; set; }
+    }
+
+    public delegate void TransitionEventHandler(object sender, TransitionEventArgs e) ;
+
+    public class NotificationHelper : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly ISynchronizeInvoke _invokeDelegate;
+        private readonly StateMachine _sm;
+
+        public NotificationHelper(ISynchronizeInvoke invokeDelegate, StateMachine sm, PropertyChangedEventHandler p)
+        {
+            this._invokeDelegate = invokeDelegate;
+            this._sm = sm;
+            this.PropertyChanged = p;
+
+            _sm.PropertyChanged += OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                if (_invokeDelegate.InvokeRequired)
+                {
+                    _invokeDelegate.Invoke(new PropertyChangedEventHandler(OnPropertyChanged),
+                                          new[] { sender, e });
+                    return;
+                }
+                PropertyChanged(this, e);
+            }
+        }
+    }
+    #endregion
+
+    #region BaseClasses
+    /// <summary>
+    /// Trigger Exception (Condition failed or more).
+    /// </summary>
+    public class GuardException : ApplicationException
+    {
+        public GuardException(int code, string msg) : base(msg)
+        {
+            Code = code ;
+        }
+
+        public int Code { get; set; }
+    }
+
+    /// <summary>
+    /// Effect Exception (Condition failed or more).
+    /// </summary>
+    public class EffectException : ApplicationException
+    {
+        public EffectException(int code, string msg) : base(msg)
+        {
+            Code = code;
+        }
+
+        public int Code { get; set; }
+    }
+
+    /// <summary>
+    /// Base class for all Triggers that start a transition between states.
+    /// </summary>
+    public abstract class TriggerBase {
+        public TriggerBase(StateMachine machine)
+        {
+            this.StateMachine = machine ;
+        }
+
+        /// <summary>
+        /// The state machine this state belongs to.
+        /// </summary>
+        public StateMachine StateMachine { get; private set; }
+    }
+
+    /// <summary>
+    /// Base class for all Triggers that start a transition between states.
+    /// </summary>
+    public abstract class GuardBase
+    {
+        public GuardBase(StateMachine machine)
+        {
+            this.StateMachine = machine;
+        }
+
+        public virtual void Execute()
+        {
+            // Will throw a GuardException when condition goes wrong
+            return;
+        }
+
+        /// <summary>
+        /// The state machine this state belongs to.
+        /// </summary>
+        public StateMachine StateMachine { get; private set; }
+    }
+    public class DefaultGuardBase : GuardBase {
+        public DefaultGuardBase(StateMachine machine) : base(machine) { }
+    };
+
+    /// <summary>
+    /// Base class for all States of the State Machine.
+    /// </summary>
+    public abstract class StateBase
+	{ 
+		/// <summary>
+		/// Creates a new instance of this state with a reference to the state machine.
+		/// </summary>
+		public StateBase(StateMachine machine, string Name)
+		{
+			this.StateMachine = machine;
+            this.Name = Name;
+			this.Initialize();
+		}
+		/// <summary>
+		/// The state machine this state belongs to.
+		/// </summary>
+		public StateMachine StateMachine { get; private set; }
+        /// <summary>
+        /// The state machine this state belongs to.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Initializes this state before the machine actually enters it.
+        /// </summary>
+        protected virtual void Initialize() 
+		{
+		}
+
+        /// <summary>
+        /// Returns a list of callable triggers
+        /// </summary>
+        public virtual List<TriggerBase> GetTriggers()
+        {
+            return new List<TriggerBase>();
+        }
+
+        /// <summary>
+        /// Is executed when the state machine enters this state.
+        /// </summary>
+        public virtual void OnEntry(TriggerBase trigger) { }
+        /// <summary>
+        /// Is executed when the state machine leaves this state.
+        /// </summary>
+        public virtual void OnExit(TriggerBase trigger) { }
+	}
+
+    /// <summary>
+    /// This state represents the start of the StateMachine.
+    /// </summary>
+    /* 
+    public partial class StartState : StateBase
+    {
+        public StartState(StateMachine machine) : base(machine, "StartState") {}
+        public override void OnEntry(TriggerBase causedByTrigger) { }
+        public override void OnExit(TriggerBase causedByTrigger) { }
+    }
+    */
+
+    /// <summary>
+    /// Base class for the state machine. Implements main functionality.
+    /// </summary>
+    public abstract partial class StateMachine : INotifyPropertyChanged, IDisposable
+    {
+		public string Name { get; protected set; } = "unknown";
+
+		/// <summary>
+		/// Creates a new instance of this state machine.
+		/// </summary>
+		public StateMachine(ISynchronizeInvoke invokeDelegate)
+		{
+            _invokeDelegate = invokeDelegate;
+            _CurrentState = null;
+			this.Initialize();
+
+            // Now start the Transition Consumer Thread
+            _queue = new Queue<TriggerBase>();
+            _syncEvents = new SyncEvents();
+            _sync = new object();
+            consumerThread = new Thread(ConsumerThread);
+            consumerThread.Start();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                _syncEvents.ExitThreadEvent.Set();
+                consumerThread.Join();
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        ~StateMachine() {
+          // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+          Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        /// <summary>
+        /// Allows custom initailization code.
+        /// </summary>
+        protected virtual void Initialize() { }
+
+        /// <summary>
+        /// Start State call.
+        /// </summary>
+        public abstract void Start() ;
+
+        /// <summary>
+        /// Makes the state machine go into another state.
+        /// </summary>
+        public void TransitionToNewState(StateBase newState, TriggerBase causedByTrigger, GuardBase guard, Func<StateBase, StateBase, TriggerBase, bool> lambda)
+		{
+            if (disposedValue)
+            { // Silent
+                throw new InvalidOperationException("State Machine Disposed");
+            }
+            // Pull the trigger to find if condition is Ok.
+            OnStartTransition(this.CurrentState, newState, causedByTrigger);
+            if ( guard != null )
+            {
+                guard.Execute();
+            }
+
+            OnBeforeExitingPreviousState(this.CurrentState, newState, causedByTrigger);
+            // exit the current state
+            if (this.CurrentState != null)
+				this.CurrentState.OnExit(causedByTrigger);
+
+            StateBase previousState = this.CurrentState ;
+			this.CurrentState = newState;
+            // OnEvent(previousState, this.CurrentState, causedByTrigger);
+            if (lambda != null)
+                lambda(previousState, this.CurrentState, causedByTrigger);
+
+            // enter the new state
+            if (this.CurrentState != null)
+				this.CurrentState.OnEntry(causedByTrigger);
+            OnEndTransition(previousState, this.CurrentState, causedByTrigger);
+        }
+
+        private ISynchronizeInvoke _invokeDelegate;
+        private StateBase _CurrentState;
+        private Thread consumerThread = null;
+
+        /// <summary>
+        /// Gets the state the state machine is currently in.
+        /// </summary>
+        public StateBase CurrentState 
+		{ 
+			get {
+                if (disposedValue)
+                { // Silent
+                    throw new InvalidOperationException("State Machine Disposed");
+                }
+                return _CurrentState;
+            }
+			private set
+			{
+				_CurrentState = value; OnPropertyChanged(this, new PropertyChangedEventArgs("CurrentState"));
+			}
+		}
+
+        /// <summary>
+        /// Tells if the State Machine is started or set properly
+        /// </summary>
+        public bool IsWorking { get {
+                if (disposedValue)
+                { // Silent
+                    throw new InvalidOperationException("State Machine Disposed");
+                }
+                return _CurrentState != null ;
+        } }
+
+        private Queue<TriggerBase> _queue;
+        private SyncEvents _syncEvents;
+        private readonly object _sync;
+
+        /// <summary>
+        /// Makes the state machine recive a command and dispatch it through the internal Queue.
+        /// </summary>
+        public void ProcessTrigger(TriggerBase trigger) {
+            if (disposedValue)
+            { // Silent
+                throw new InvalidOperationException("State Machine Disposed");
+            }
+            lock (((ICollection)_queue).SyncRoot)
+            {
+                 _queue.Enqueue(trigger);
+                 _syncEvents.NewTransitionEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// Makes the state machine processo a command. Depending on its current state
+        /// and the designed transitions the machine reacts to the trigger.
+        /// </summary>
+        protected abstract void ProcessTriggerInternal(TriggerBase trigger);
+
+        /// <summary>
+        /// Internal Transition Thread.
+        /// </summary>
+        private void ConsumerThread()
+        {
+            while (WaitHandle.WaitAny(_syncEvents.EventArray) != 1)
+            {
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    TriggerBase t = _queue.Dequeue();
+                    ProcessTriggerInternal(t) ;
+                }
+            }
+            // Console.WriteLine("Consumer Thread: consumed {0} items", count);
+        }
+
+        protected void OnStartTransition(StateBase prev, StateBase next, TriggerBase why)
+        {
+            if (this.StartTransition != null)
+                this.StartTransition(this, new TransitionEventArgs(prev, next, why));
+        }
+        public event TransitionEventHandler StartTransition;
+
+        protected void OnBeforeExitingPreviousState(StateBase prev, StateBase next, TriggerBase why)
+        {
+            if (this.BeforeExitingPreviousState != null)
+                this.BeforeExitingPreviousState(this, new TransitionEventArgs(prev, next, why));
+        }
+        public event TransitionEventHandler BeforeExitingPreviousState;
+
+        protected void OnEndTransition(StateBase prev, StateBase next, TriggerBase why)
+        {
+            if (this.EndTransition != null)
+                this.EndTransition(this, new TransitionEventArgs(prev, next, why));
+        }
+        public event TransitionEventHandler EndTransition;
+
+        protected void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+//            if (this.PropertyChanged != null)
+//                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            if (PropertyChanged != null)
+            {
+                if (_invokeDelegate.InvokeRequired)
+                {
+                    _invokeDelegate.Invoke(new PropertyChangedEventHandler(PropertyChanged),
+                                          new[] { sender, e });
+                    return;
+                }
+                PropertyChanged(this, e);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+	#endregion
+}
+// End of Tempate
+
---
+++ core/statemachine/test/TestFormBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xwcs.core.statemachine;
+
+namespace xwcs.core.statemachine.test
+{
+    public partial class TestFormBase : Form
+    {
+
+		private StateMachine _machine;
+		
+		protected virtual StateMachine CreateMachine()
+		{
+			return null; //must be implemented!
+		}
+
+		public TestFormBase()
+        {
+            InitializeComponent();
+			//DocState = new DocumentState(this);
+			_machine = CreateMachine();
+
+			Text = "State machine : " + _machine.Name;
+
+			this.FormClosing += (s, e) =>
+            {
+				_machine.Dispose();
+            };
+
+			//_propertyChangedNH = new xwcs.core.statemachine.NotificationHelper(this, DocState, DocState_PropertyChanged);
+			_machine.PropertyChanged += DocState_PropertyChanged;
+        }
+
+        private List<Button> _myButtons = new List<Button>() ;
+        // private xwcs.core.statemachine.NotificationHelper _propertyChangedNH;
+
+        private void AddButtons()
+        {
+            foreach (Button b in _myButtons)
+            {
+                panel1.Controls.Remove(b);
+                b.Dispose(); // As delete.
+            }
+
+            _myButtons.Clear();
+
+            // Ask for available triggers.
+            List<TriggerBase> tList = _machine.CurrentState.GetTriggers();
+
+            foreach (TriggerBase t in tList)
+            {
+                Button newButton = new Button();
+                newButton.Text = t.GetType().Name;
+                newButton.Click += (s, e) =>
+                {
+					_machine.ProcessTrigger(t);
+                } ;
+                _myButtons.Add(newButton);
+                panel1.Controls.Add(newButton);
+            }
+        }
+
+        private void DocState_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if ( e.PropertyName == "CurrentState")
+            {
+                label1.Text = _machine.CurrentState.Name;
+                AddButtons();
+            }
+            // else don't care
+        }
+
+        /*
+        private void DocState_EventChanged(object sender, TransitionEventArgs e)
+        {
+            if (e.Prev is CorrezioneState && e.Next is ConsolidatoState)
+            {
+                var confirmResult = MessageBox.Show("Pay attention, Event from CorrezioneState to ConsolidatoState occurred",
+                        "Event Handler", MessageBoxButtons.OK);
+            }
+            // else don't care
+        }
+        */
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			_machine.Start();
+        }
+    }
+}
