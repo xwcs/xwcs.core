2eb116069346e0459b4641fd51fc512f9b3239c0
Ladislav Sopko <lsopko@3di.it>
2016-05-09 09:32:33 +0200

    dev in progress

--- core/db/binding/attributes/CustomAttribute.cs
+++ core/db/binding/attributes/CustomAttribute.cs
@@ -9,5 +9,7 @@ namespace xwcs.core.db.binding.attributes
 		public virtual void applyRetrievingAttribute(IDataLayoutExtender host, FieldRetrievingEventArgs e) { }
 		public virtual void applyRetrievedAttribute(IDataLayoutExtender host, FieldRetrievedEventArgs e) { }
 		public virtual void applyGridColumnPopulation(IDataGridSource host, string ColumnName) { }
+		public virtual void attach() { }
+		public virtual void dettach() { }
 	}
 }
