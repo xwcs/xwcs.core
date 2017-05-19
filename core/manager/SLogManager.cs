d37bf012ad5a786a28947d4c9b5f7d7ac3e3f765
Ladislav Sopko <lsopko@3di.it>
2017-05-19 12:53:26 +0200

    Added async logging

--- core/manager/SLogManager.cs
+++ core/manager/SLogManager.cs
@@ -9,20 +9,77 @@ using System.Runtime.CompilerServices;
 using log4net;
 using System.Diagnostics;
 using System.Reflection;
+using System.Threading;
+using System.Collections;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace xwcs.core.manager
 {
-	public interface ILogger {
+    #region SyncClasses
+    /// <summary>
+    /// Synchronization Class.
+    /// </summary>
+    public class SyncEvents
+    {
+        public SyncEvents()
+        {
+
+            _newTransitionEvent = new AutoResetEvent(false);
+            _exitThreadEvent = new ManualResetEvent(false);
+            _eventArray = new WaitHandle[2];
+            _eventArray[0] = _newTransitionEvent;
+            _eventArray[1] = _exitThreadEvent;
+        }
+
+        public EventWaitHandle ExitThreadEvent
+        {
+            get { return _exitThreadEvent; }
+        }
+        public EventWaitHandle NewTransitionEvent
+        {
+            get { return _newTransitionEvent; }
+        }
+        public WaitHandle[] EventArray
+        {
+            get { return _eventArray; }
+        }
+
+        private EventWaitHandle _newTransitionEvent;
+        private EventWaitHandle _exitThreadEvent;
+        private WaitHandle[] _eventArray;
+    }
+    #endregion
+
+
+    public interface ILogger : IDisposable
+    {
 		void Debug(string msg);
 		void Info(string msg);
 		void Warn(string msg);
 		void Error(string msg);
 		void Fatal(string msg);
 	}
+
+    public enum LogKind
+    {
+        N, // no log
+        D,
+        I,
+        W,
+        E,
+        F
+    } 
+
+    public struct LogMessage
+    {
+        public LogKind Kind;
+        public string Message;
+        public string Method;
+        public int Line;
+    }
 	
-    public class SLogManager : ILogger
+    public class SLogManager : ILogger, IDisposable
     {
         private static SLogManager instance;
 		private ILogger global = null;
@@ -35,59 +92,197 @@ namespace xwcs.core.manager
 			private static SEventProxy _proxy;
             private ILog logger = null;
 
-			public SimpleLogger() : this("Global")
-			{
-			}
+            private Queue<LogMessage> _queue = null;
+            private SyncEvents _syncEvents = null;
+            private Thread _consumerThread = null;
 
-			public SimpleLogger(string name)
+            public SimpleLogger() : this("Global")
 			{
+                
+            }
+
+            /// <summary>
+            /// Internal Transition Thread.
+            /// </summary>
+            private void ConsumerThread()
+            {
+                while (WaitHandle.WaitAny(_syncEvents.EventArray) != 1 && !disposedValue)
+                {
+                    bool GoWait = false;
+                    LogMessage t = new LogMessage() { Kind = LogKind.N, Message = "", Method = "", Line = 0 };
+
+                    while (!GoWait && !disposedValue)
+                    {
+                        // in lock just work with queue
+
+                        int cnt = 10;
+                        bool done = false;
+
+                        do
+                        {
+                            if (System.Threading.Monitor.TryEnter(((ICollection)_queue).SyncRoot, 5000))
+                            {
+                                try
+                                {
+                                    if (_queue.Count > 0)
+                                    {
+                                        t = _queue.Dequeue();
+                                    }
+                                    else
+                                    {
+                                        GoWait = true;
+                                    }
+                                    done = true;
+                                }
+                                finally
+                                {
+                                    System.Threading.Monitor.Exit(((ICollection)_queue).SyncRoot);
+                                }
+                            }
+
+                        } while (cnt-- > 0 && !done);
+
+                        if (cnt <= 0) throw new ApplicationException("Cant lock logger queue!");
+                        
+                        // now log if there is something
+                        if(!GoWait && !disposedValue)
+                        {
+                            
+                            switch (t.Kind)
+                            {
+                                case LogKind.D:
+                                    _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("[{2}]{0} - {1}", logger.Logger.Name, t.Message, t.Kind.ToString()) }));
+                                    logger.Warn(string.Format("{0}", t.Message));
+                                    break;
+                                case LogKind.E:
+                                    _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("[{3}]{0} - {1} - {2}({4})", logger.Logger.Name, t.Message, t.Method, t.Kind.ToString(), t.Line) }));
+                                    logger.Error(string.Format("{0} in : {1}({2})", t.Message, t.Method, t.Line));
+                                    break;
+                                case LogKind.F:
+                                    _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("[{3}]{0} - {1} - {2}({4})", logger.Logger.Name, t.Message, t.Method, t.Kind.ToString(), t.Line) }));
+                                    logger.Fatal(string.Format("{0} in : {1}({2})", t.Message, t.Method, t.Line));
+                                    break;
+                                case LogKind.I:
+                                    _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("[{2}]{0} - {1}", logger.Logger.Name, t.Message, t.Kind.ToString()) }));
+                                    logger.Info(string.Format("{0}", t.Message));
+                                    break;
+                                case LogKind.W:
+                                    _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("[{2}]{0} - {1}", logger.Logger.Name, t.Message, t.Kind.ToString()) }));
+                                    logger.Warn(string.Format("{0}", t.Message));
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            public SimpleLogger(string name)
+			{
+
+
 				_proxy = SEventProxy.getInstance();
 				logger = LogManager.GetLogger(name);
-			}
 
-			public SimpleLogger(Type t) : this(t.Name) { }
+                _queue = new Queue<LogMessage>();
+                _syncEvents = new SyncEvents();
+                _consumerThread = new Thread(ConsumerThread);
+                _consumerThread.Start();
+
+            }
+
+			public SimpleLogger(Type t) : this(t.Name) {
+
+                
+            }
 
 			public void Debug(string msg)
 			{
 				if (!logger.IsDebugEnabled) return;
-				_proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("{0} - {1}", logger.Logger.Name, msg) }));
-				logger.Debug(msg);
-			}
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    _queue.Enqueue(new LogMessage() { Kind= LogKind.D, Message= msg});
+                }
+                _syncEvents.NewTransitionEvent.Set();
+            }
 
 			public void Info(string msg)
 			{
-				if (!logger.IsInfoEnabled) return;
-				_proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("{0} - {1}", logger.Logger.Name, msg) }));
-				logger.Info(msg);
-			}
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    _queue.Enqueue(new LogMessage() { Kind = LogKind.I, Message = msg });
+                }
+                _syncEvents.NewTransitionEvent.Set();
 
-			public void Warn(string msg)
+            }
+
+            public void Warn(string msg)
 			{
-				if (!logger.IsWarnEnabled) return;
-				_proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("{0} - {1}", logger.Logger.Name, msg) }));
-				logger.Warn(msg);
-			}
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    _queue.Enqueue(new LogMessage() { Kind = LogKind.W, Message = msg });
+                }
+                _syncEvents.NewTransitionEvent.Set();
 
-			public void Error(string msg)
+            }
+
+            public void Error(string msg)
 			{
 				if (!logger.IsErrorEnabled) return;
 
-                MethodBase info = new StackFrame(1).GetMethod();
-                // Get the line number from the stack frame
-                _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("{0} - {1} - {2}", logger.Logger.Name, msg, info.Name) }));
-				logger.Error(string.Format("{0} in : {1}", msg, info.Name));
-			}
+                StackFrame sf = new StackFrame(1);
 
-			public void Fatal(string msg)
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    _queue.Enqueue(new LogMessage() { Kind = LogKind.E, Message = msg, Method = sf.GetMethod().Name, Line = sf.GetFileLineNumber() });
+                }
+                _syncEvents.NewTransitionEvent.Set();
+
+            }
+
+            public void Fatal(string msg)
 			{
 				if (!logger.IsFatalEnabled) return;
 
-                MethodBase info = new StackFrame(1).GetMethod();
+                StackFrame sf = new StackFrame(1);
 
-                _proxy.fireEvent(new OutputMessageEvent(this, new OutputMessage { Message = string.Format("{0} - {1} - {2}", logger.Logger.Name, msg, info.Name) }));
-				logger.Fatal(string.Format("{0} in : {1}", msg, info.Name));
-			}
-		}
+                lock (((ICollection)_queue).SyncRoot)
+                {
+                    _queue.Enqueue(new LogMessage() { Kind = LogKind.F, Message = msg, Method = sf.GetMethod().Name, Line = sf.GetFileLineNumber() });
+                }
+                _syncEvents.NewTransitionEvent.Set();
+            }
+
+            #region IDisposable Support
+            private bool disposedValue = false; // Per rilevare chiamate ridondanti
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        // managed things here
+                    }
+                    //thread must be killed any way
+                    _syncEvents.ExitThreadEvent.Set();
+                    _consumerThread.Join();
+
+                    disposedValue = true;
+                }
+            }
+
+            // Questo codice viene aggiunto per implementare in modo corretto il criterio Disposable.
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            ~SimpleLogger()
+            {
+                Dispose(false);
+            }
+            #endregion
+        }
 
 
 		//singleton need private ctor
@@ -144,5 +339,38 @@ namespace xwcs.core.manager
 		{
 			global.Fatal(msg);
 		}
-	}
+
+        #region IDisposable Support
+        private bool disposedValue = false; // Per rilevare chiamate ridondanti
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    //managed here
+                }
+
+                foreach (ILogger l in _loggers.Values)
+                {
+                    l.Dispose();
+                }
+                global.Dispose();
+
+                disposedValue = true;
+            }
+        }
+
+        ~SLogManager() {
+           Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
 }
