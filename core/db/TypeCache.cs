495c69126947b61231d5b7ff02231e820d93d0e2
Ladislav Sopko <lsopko@3di.it>
2017-09-06 07:37:27 +0200

    Moved classes out from EntityBase

---
+++ core/db/TypeCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xwcs.core.db
+{
+    public class TypeCacheData
+    {
+        public Dictionary<string, MethodInfo> Getters = new Dictionary<string, MethodInfo>();
+        public Dictionary<string, MethodInfo> Setters = new Dictionary<string, MethodInfo>();
+        public Dictionary<string, PropertyInfo> Properties = new Dictionary<string, PropertyInfo>();
+        public Dictionary<Type, IEnumerable<PropertyInfo>> PropertiesForAttribCache = new Dictionary<Type, IEnumerable<PropertyInfo>>();
+    }
+
+    public static class TypeCache
+    {
+        public static Dictionary<Type, TypeCacheData> _cache = new Dictionary<Type, TypeCacheData>();
+
+        public static TypeCacheData GetTypeCacheData(Type t)
+        {
+            TypeCacheData ret;
+            if (!_cache.TryGetValue(t, out ret))
+            {
+                ret = new TypeCacheData();
+                _cache.Add(t, ret);
+            }
+
+            return ret;
+        }
+    }
+}
