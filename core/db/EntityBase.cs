ee90acdc8750b4926258edb42211078d6efd87bc
Ladislav Sopko <lsopko@3di.it>
2017-09-06 08:32:14 +0200

    work in progress

--- core/db/EntityBase.cs
+++ core/db/EntityBase.cs
@@ -927,30 +927,6 @@ namespace xwcs.core.db
         }
     }
 
-    public class TypeCacheData
-    {
-        public Dictionary<string, MethodInfo> Getters = new Dictionary<string, MethodInfo>();
-        public Dictionary<string, MethodInfo> Setters = new Dictionary<string, MethodInfo>();
-        public Dictionary<string, PropertyInfo> Properties = new Dictionary<string, PropertyInfo>();
-        public Dictionary<Type, IEnumerable<PropertyInfo>> PropertiesForAttribCache = new Dictionary<Type, IEnumerable<PropertyInfo>>();
-    }
-
-    public static class TypeCache{
-        public static Dictionary<Type, TypeCacheData> _cache = new Dictionary<Type, TypeCacheData>();
-
-        public static TypeCacheData GetTypeCacheData(Type t)
-        {
-            TypeCacheData ret;
-            if(!_cache.TryGetValue(t, out ret))
-            {
-                ret = new TypeCacheData();
-                _cache.Add(t, ret);
-            }
-
-            return ret;
-        }
-    }
-
     internal static class TConvert
     {
         public static T ChangeType<T>(object value)
