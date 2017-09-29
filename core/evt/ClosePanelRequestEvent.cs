d77459779b62179be5e079c0b22f484690e84eab
PavolPala <palap@ipoint.sk>
2017-09-29 16:02:16 +0200

    Work on progress

---
+++ core/evt/ClosePanelRequestEvent.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using DevExpress.XtraBars.Docking;
+using DevExpress.XtraEditors;
+using xwcs.core.controls;
+
+namespace xwcs.core.evt
+{
+	//This is very simply event, no data needed only sender (VisualControl)!
+	public class ClosePanelRequest
+    {      
+	}
+
+    public class ClosePanelRequestEvent : Event
+    {
+        public ClosePanelRequestEvent(object sender) : base(sender, EventType.ClosePanelRequestEvent, null)
+        {
+        }
+    }
+}
